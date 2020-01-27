45213f1568aad6984cfb0ca27638dc8e4e1f9505
Causevic <selma.causevic@tno.nl>
2020-01-27 12:58:16 +0100

    Second version of tutorials

--- influxdbreader.py
+++ influxdbreader.py
@@ -15,7 +15,7 @@ def get_influx_db_demand_profile():
 # Returns an InfluxDB object of the typical solar production from Energy Data Repository
 # The ID is passed to query the InfluxDB (see read_yearly_solar_production_profile_data(year) method)
 def get_influx_db_solar_profile():
-    return edrreader.get_item_from_EDR('507a0a51-035f-4adb-b53f-21b9a676da00')
+    return edrreader.get_item_from_EDR('55ef73b3-fa4f-49e8-8ad6-2b1801c25dfe')
 
 
 # Gets the profile as a dictionary
@@ -50,28 +50,28 @@ def get_path(influx_db_profile):
 
 
 # Reads the InfluxDB to get the required profiles for a specified year
-def read_yearly_profile_data(influx_db_profile, table_name, year):
+def read_yearly_profile_data(influx_db_profile, year):
 
     db_client = DataFrameClient(ssl=True, host=get_host(influx_db_profile), port=443, path=get_path(influx_db_profile), database=influx_db_profile.database)
 
-    query = 'SELECT * FROM "' + table_name + '" WHERE time >= \''+str(year)+'-01-01T00:00:00Z\' AND time < \''+str(year+1)+'-01-01T00:00:00Z\''
+    query = 'SELECT * FROM "' + influx_db_profile.measurement + '" WHERE time >= \''+str(year)+'-01-01T00:00:00Z\' AND time < \''+str(year+1)+'-01-01T00:00:00Z\''
 
     query_result_dict = db_client.query(query)
 
     # Returns a dataframe of the query results
-    query_result_df = query_result_dict.get(table_name)
+    query_result_df = query_result_dict.get(influx_db_profile.measurement)
 
     return query_result_df
 
 
 # Queries Influx db to get the typical household yearly profile
 def read_yearly_demand_profile_data(year):
-    yearly_demand_profile = read_yearly_profile_data(get_influx_db_demand_profile(), "nedu_elektriciteit_2015-2018", year)
+    yearly_demand_profile = read_yearly_profile_data(get_influx_db_demand_profile(), year)
     # Returns the demand profile of an average household in The Netherlands
     # return yearly_demand_profile['E1A']
     return yearly_demand_profile
 
 
 def read_yearly_solar_production_profile_data(year):
-    yearly_production_profile = read_yearly_profile_data(get_influx_db_solar_profile(), "solar_relative_2011-2016", year)
+    yearly_production_profile = read_yearly_profile_data(get_influx_db_solar_profile(), year)
     return yearly_production_profile
